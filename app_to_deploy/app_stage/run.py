741d85c03c1c866afe89f78243307717b780e331
LenaMartinson <lmartinson@yandex.ru>
2024-05-14 16:42:39 +0300

    small fixes

--- app_to_deploy/app_stage/run.py
+++ app_to_deploy/app_stage/run.py
@@ -108,7 +108,7 @@ def test(raw_text, model_func):
                             ).to(args.device)
     
     if args.upload_model is not None:
-        base_model = torch.load(args.upload_model, map_location=torch.device('cpu'))
+        base_model = torch.load(args.upload_model)
         base_model.eval()
     
     test_dataset = ASTE_End2End_Dataset(raw_text,
